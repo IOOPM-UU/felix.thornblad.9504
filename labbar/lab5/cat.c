b55788c1544136e5884915bad0c8dedf8e3c4116
Felix-Thornblad <felix.thornblad@gmail.com>
2020-09-10 15:07:02 +0200

    Add files via upload

---
+++ labbar/lab5/cat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void cat(char *filename)
+{
+  FILE *f = fopen(filename, "r");
+  int c = fgetc(f);
+  int counter = 1;
+  fprintf(stdout, "%d ", counter);
+  while (c != EOF)
+  {
+    fputc(c, stdout);
+    if(c == '\n')
+    {
+      counter++;
+      fprintf(stdout, "%d ", counter);
+    }
+    c = fgetc(f);
+  }
+  fputc('\n', stdout);
+  fclose(f);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    fprintf(stdout, "Usage: %s fil1 ...\n", argv[0]);
+  }
+  else
+  {
+    for (int i = 1; i < argc; ++i)
+    {
+      cat(argv[i]);
+    }
+  }
+
+  return 0;
+}
